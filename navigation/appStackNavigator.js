60e98cd0a3e810888442b9666f14d9f94787c578
Jasper Huang <jasper@expensify.com>
2022-04-05 00:26:16 -0700

    add home tab navigator to app stack navigator

--- navigation/appStackNavigator.js
+++ navigation/appStackNavigator.js
@@ -3,6 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Landing from '../screens/landing';
 import SignUp from '../screens/signup/signup';
 import SignupStackNavigator from '../screens/signup/signup';
+import HomeTabNavigator from './homeTabNavigator';
 
 export default function AppStackNavigator (props) {
     const Stack = createNativeStackNavigator();
@@ -22,6 +23,11 @@ export default function AppStackNavigator (props) {
             <Stack.Screen
                 name="SignUp"
                 component={SignupStackNavigator}
+                options={{}}
+            />
+            <Stack.Screen
+                name="Home"
+                component={HomeTabNavigator}
                 options={{}}
             />
         </Stack.Navigator>
