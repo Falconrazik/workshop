996531888cb610f50c324779aec3105a7f761972
Jasper Huang <jasper@expensify.com>
2022-04-05 01:12:25 -0700

    use display none and add comment

--- screens/discover/discover.js
+++ screens/discover/discover.js
@@ -10,18 +10,18 @@ export default function Discover(props) {
     const [currentTab, setCurrentTab] = React.useState(CONST.DISCOVER_TABS.SHORTS);
 
     // Using display: 'none' instead of conditional rendering so that the pages don't un-render,
-    // since we want the user's place to be saved when they navigate between the two pages.
+    // since we want the user's scroll position/route to be saved when they navigate between the two pages.
     return (
         <SafeAreaView>
             <DiscoverTabsHeader currentTab={currentTab} onTabPress={setCurrentTab} />
             {currentTab === CONST.DISCOVER_TABS.CREATORS &&
                 <DiscoverCreators
-                    containerStyles={currentTab === CONST.DISCOVER_TABS.CREATORS ? {display: 'none'} : {}}
+                    containerStyles={currentTab === CONST.DISCOVER_TABS.CREATORS ? {} : {display: 'none'}}
                 />
             }
             {currentTab === CONST.DISCOVER_TABS.SHORTS &&
                 <DiscoverShorts
-                    containerStyles={currentTab === CONST.DISCOVER_TABS.SHORTS ? {display: 'none'} : {}}
+                    containerStyles={currentTab === CONST.DISCOVER_TABS.SHORTS ? {} : {display: 'none'}}
                 />
             }
         </SafeAreaView>
