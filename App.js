773af91c9c420db1588e9552519066375b46fd2b
Jasper Huang <jasper@expensify.com>
2022-04-05 23:04:05 -0700

    wip

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import {View} from 'react-native';
 import AppStackNavigator from './navigation/appStackNavigator';
 import {StatusBar} from 'expo-status-bar';
+import Constants from 'expo-constants';
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <AppStackNavigator/>
-        </NavigationContainer>
+        <>
+            <View style={{height: Constants.statusBarHeight, backgroundColor: 'black'}}>
+                <StatusBar translucent style="light" />
+            </View>
+            <NavigationContainer>
+                <AppStackNavigator/>
+            </NavigationContainer>
+        </>
     );
 }
