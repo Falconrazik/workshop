ed573e8b9409381edf48f8fa492f831d9eeeb88d
Jasper Huang <jasper@expensify.com>
2022-04-26 12:53:47 -0700

    add content val found (but it's commented out since it has tiktok ui)

--- videos.js
+++ videos.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 const videos = [
     {
         file: require('./assets/videos/humphreytalks/humphreytalks_1.mp4'),
@@ -89,6 +91,11 @@ const videos = [
         creatorUID: 'LzuHXQROAEWpuFHUFTY1CbXzWuV2',
         thumbnail: require('./assets/videos/johnefinance/johnefinance_1.png'),
     },
+    // {
+    //     file: require('./assets/videos/jay.iverson/jay.iverson_2.mp4'),
+    //     creatorUID: 'wUGdTJJfLLvf0mxT87zj',
+    //     thumbnail: require('./assets/videos/jay.iverson/jay.iverson_2.png')
+    // },
     {
         file: require('./assets/videos/humphreytalks/humphreytalks_3.mp4'),
         creatorUID: 'eqcLHaUvgEX2BtBJmXrojzg16eI3',
@@ -134,11 +141,26 @@ const videos = [
         creatorUID: 'LhpizUPucfNZnliKL937VjzurSj1',
         thumbnail: require('./assets/videos/johnsfinancetips/johnsfinancetips_3.png'),
     },
+    // {
+    //     file: require('./assets/videos/vanity_makeup/vanity_makeup_2.mp4'),
+    //     creatorUID: 'LFrt0j6DJk1stE1t90QP',
+    //     thumbnail: require('./assets/videos/vanity_makeup/vanity_makeup_2.png')
+    // },
+    // {
+    //     file: require('./assets/videos/jay.iverson/jay.iverson_3.mp4'),
+    //     creatorUID: 'wUGdTJJfLLvf0mxT87zj',
+    //     thumbnail: require('./assets/videos/jay.iverson/jay.iverson_3.png')
+    // },
     {
         file: require('./assets/videos/nylenayga/nylanayga_2.mp4'),
         creatorUID: 'kRd4ZqIqlQNxhImyqiM6',
         thumbnail: require('./assets/videos/nylenayga/nylanayga_2.png'),
     },
+    // {
+    //     file: require('./assets/videos/vanity_makeup/vanity_makeup_3.mp4'),
+    //     creatorUID: 'LFrt0j6DJk1stE1t90QP',
+    //     thumbnail: require('./assets/videos/vanity_makeup/vanity_makeup_3.png')
+    // },
     {
         file: require('./assets/videos/_joeandrews/_joeandrews_2.mp4'),
         creatorUID: 'LnGnI2tlmJTFbCiNxP9f',
@@ -170,6 +192,48 @@ const videos = [
         creatorUID: '8Dpv8bR8lzMKVev9W2dRo3doBun1',
         thumbnail: require('./assets/videos/richinvesting/richinvesting_1.png'),
     },
+    // {
+    //     file: require('./assets/videos/vanity_makeup/vanity_makeup_1.mp4'),
+    //     creatorUID: 'LFrt0j6DJk1stE1t90QP',
+    //     thumbnail: require('./assets/videos/vanity_makeup/vanity_makeup_1.png')
+    // },
+    // {
+    //     file: require('./assets/videos/jay.iverson/jay.iverson_1.mp4'),
+    //     creatorUID: 'wUGdTJJfLLvf0mxT87zj',
+    //     thumbnail: require('./assets/videos/jay.iverson/jay.iverson_1.png')
+    // },
+    // {
+    //     file: require('./assets/videos/jyooonie/jyooonie_1.mp4'),
+    //     creatorUID: '4kmBavbvuiYrvnBd4ATc',
+    //     thumbnail: require('./assets/videos/jyooonie/jyooonie_1.png')
+    // },
+    // {
+    //     file: require('./assets/videos/jyooonie/jyooonie_2.mp4'),
+    //     creatorUID: '4kmBavbvuiYrvnBd4ATc',
+    //     thumbnail: require('./assets/videos/jyooonie/jyooonie_2.png')
+    // },
+    // {
+    //     file: require('./assets/videos/jyooonie/jyooonie_3.mp4'),
+    //     creatorUID: '4kmBavbvuiYrvnBd4ATc',
+    //     thumbnail: require('./assets/videos/jyooonie/jyooonie_3.png')
+    // },
+    // {
+    //     file: require('./assets/videos/kennnnito/kennnnito_1.mp4'),
+    //     creatorUID: 'WrWec4sd6Hm4NPWZYWZR',
+    //     thumbnail: require('./assets/videos/kennnnito/kennnnito_1.png')
+    // },
+    // {
+    //     file: require('./assets/videos/kennnnito/kennnnito_2.mp4'),
+    //     creatorUID: 'WrWec4sd6Hm4NPWZYWZR',
+    //     thumbnail: require('./assets/videos/kennnnito/kennnnito_2.png')
+    // },
+    // {
+    //     file: require('./assets/videos/kennnnito/kennnnito_3.mp4'),
+    //     creatorUID: 'WrWec4sd6Hm4NPWZYWZR',
+    //     thumbnail: require('./assets/videos/kennnnito/kennnnito_3.png')
+    // },
+
+
 ];
 
-export default videos;
+export default _.shuffle(videos);
